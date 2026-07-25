2f7bb0a0287730fa8b26c4dccf111f106bfc6e54
Tybo Verslype <tybo.verslype@ugent.be>
2026-07-25 18:04:18 +0200

    fix: fixed prices of raspberry jam being the ones of blackberry jam

--- calculate_price.py
+++ calculate_price.py
@@ -66,7 +66,7 @@ def find_first_multivalue_discriminator(report):
 
 item_measurements = {
     "Meat Bucket":{},
-    "Raspberry Jam":{1: 388, 2: 777, 3: 1166, 4: 1555},
+    "Raspberry Jam":{1: 252, 2: 504, 3: 756, 4: 1008, 5: 1260, 6: 1512, 7: 1764},
     "Lobster Soup": {1: 612, 2: 1224, 3: 1836, 4: 2448, 5: 3060, 6: 3672, 7: 4284, 8: 4896},
 }
 
