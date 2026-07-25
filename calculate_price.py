b7378dd31511f944fa974213b6e195c4015b1136
Tybo Verslype <tybo.verslype@ugent.be>
2026-07-25 20:18:21 +0200

    chore: added level 49 items

--- calculate_price.py
+++ calculate_price.py
@@ -68,6 +68,10 @@ item_measurements = {
     "Meat Bucket":{},
     "Raspberry Jam":{1: 252, 2: 504, 3: 756, 4: 1008, 5: 1260, 6: 1512, 7: 1764},
     "Lobster Soup": {1: 612, 2: 1224, 3: 1836, 4: 2448, 5: 3060, 6: 3672, 7: 4284, 8: 4896},
+    "Pumpkin Soup": {},
+    "Rustic Bouquet": {},
+    "Asparagus": {},
+    "Asparagus Quiche": {}
 }
 
 def run_report():
