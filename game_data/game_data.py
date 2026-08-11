88edd868a96aa0b9101ea54a3496485e9891cfaa
Tybo Verslype <tybo.verslype@ugent.be>
2026-08-11 20:08:08 +0200

    chore: updated defaults

--- game_data/game_data.py
+++ game_data/game_data.py
@@ -20,7 +20,7 @@ from .fields_data import FARM_FIELDS
 
 DIAMOND_COST = 10000.0
 MAX_LEVEL = 126
-CURRENT_LEVEL = 53
+CURRENT_LEVEL = 54
 
 # Combine all inventory-holding items together using the dictionary update operator (|)
 ITEMS = {}
